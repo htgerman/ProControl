7c3047af919f3fc8629a8b1a02c6643f6ec75695
Anke Herrmann <herrmann.anke@gmail.com>
2011-05-17 13:29:04 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProControl::Application.config.secret_token = 'e40840171ad166aa82c70934cec3432029181a0c74d182e86e89f0af7621145aabbf1f59e67edb241f9eb7a248073117c35b0c724988cdc2bc8b14a7daabc4ba'
